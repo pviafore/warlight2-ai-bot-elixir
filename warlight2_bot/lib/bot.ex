dfa41b5a03edfdaf83640d0bb4e36a21c43fd0dc
Pat Viafore <patviafore@gmail.com>
2015-09-29 23:05:25 -0500

    Moving to CustomStrategy

--- warlight2_bot/lib/bot.ex
+++ warlight2_bot/lib/bot.ex
@@ -2,7 +2,7 @@ defmodule Bot do
    def main(_) do
 
       outputter = CommandOutputter.start()
-      strategy = RandomStrategy.start(outputter)
+      strategy = CustomStrategy.start(outputter)
       logic = SimpleGameLogic.start(strategy)
       command_parser = CommandParser.start(logic)
       run_input_loop( command_parser)
